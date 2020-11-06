1bc0e3702784856eac83d7b46010e43452d1f95f
Phillip Jennings <pjennings24@biomedscienceacademy.org>
2020-11-06 01:47:30 +0000

    Wrote the code

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 """
 MadLibs
-Author: 
-Period/Core:
-
+Author: Phillip Jennings
+Period/Core: Core 2
 
 """
+print("Let's play Silly Sentences!\n ")
+
+name = input("Input a name: ")
+adverb = input("Input an adverb: ")
+adjective_one = input("Input an adjective: ")
+adjective_two = input("Input another adjective: ")
+food_one = input("Input a food: ")
+food_two = input("Input another food: ")
+noun = input("Input a noun: ")
+place = input("Input a place: ")
+verb = input("Input a verb: ")
+print("\n")
+
+print(f"\"{name}! Come down the stairs {adverb}!\n Mom made {adjective_one} {food_one}s just for you!\n She says that you need to eat up for your {adjective_two} journey to the {place}.\n Honestly, I don't understand why you get to go somewhere.\n I'm always the one doing the {verb}ing around here.\n Remember that time you put a {noun} inside of a {food_two}?\n I was the one cleaning that up!\n {name}, you should really take better care of yourself.\n Well anywho, Mom said I get to go to a {place}-like area next time\"")
