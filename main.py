93048cdffec0f40a48653880f01008c9868df0fd
Phillip Jennings <pjennings24@biomedscienceacademy.org>
2020-11-09 15:45:15 +0000

    add text in first print. created second print statement that added strings. checked grammar and spelling in both print statements and input text

--- main.py
+++ main.py
@@ -15,6 +15,6 @@ food_two = input("Input another food: ")
 noun = input("Input a noun: ")
 place = input("Input a place: ")
 verb = input("Input a verb: ")
-print("\n")
 
-print(f"\"{name}! Come down the stairs {adverb}!\n Mom made {adjective_one} {food_one}s just for you!\n She says that you need to eat up for your {adjective_two} journey to the {place}.\n Honestly, I don't understand why you get to go somewhere.\n I'm always the one doing the {verb}ing around here.\n Remember that time you put a {noun} inside of a {food_two}?\n I was the one cleaning that up!\n {name}, you should really take better care of yourself.\n Well anywho, Mom said I get to go to a {place}-like area next time\"")
+print(f"\n  \"{name}! Come in here {adverb}! Mom made {adjective_one} {food_one}s just for you!\nShe says that you need to eat up for your {adjective_two} journey to {place}.\nWhy do you get to go on an adventure?\nI'm always the one doing the {verb} around here.\nRemember that time you destroyed a {noun} because you didn't like {food_two}?\nMom made me clean that up!\nI just wish you didn't have to go, {name}.\nIt's going to be a lot less loud around here without you.")
+print("But hey, at least you have" + food_one + " to help you on your journey.\"")
